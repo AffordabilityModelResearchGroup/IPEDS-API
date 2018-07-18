41b74cc8baaac1afc1f37e018f9968eabd06d554
apw6 <watsonap6@gmail.com>
2018-07-17 17:09:00 -0700

    Added psycopg2 module to requirements and the f_f1a view column list file.

---
+++ view_column_names/f_f1a.py
@@ -0,0 +1,4 @@
+column_list = [("unitid", "institution_id"),
+               ("year", "fiscal_year"),
+               ("f1b11", "state_appropriation"),
+               ("f1b12", "local_appropriation")]
